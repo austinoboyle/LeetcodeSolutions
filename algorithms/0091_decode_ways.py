0f0d82d8ad10e0acc26d9893d0f737a2150d6b65
austinoboyle <14arob@queensu.ca>
2018-11-13 22:33:05 -0500

    Update decode_ways to better dp solution T/S O(n)/O(1)

--- algorithms/0091_decode_ways.py
+++ algorithms/0091_decode_ways.py
@@ -27,58 +27,23 @@ cache = {}
 
 class Solution:
     """
-    Facts:
-
-    - Any string that has a digit > 2 right before a 0 has 0 possibilities.
-    This is because neither {3-9}0 nor anything starting with 0 are valid.
-    - Any string starting with a zero is invalid, return 0
-    - Any number of length 2 has two possibilities if it is <= 26 and not 10 or
-      20. Otherwise it only has one possibility
-    - Any non-zero digit has 1 possibility
-
-    Resursive intuition:
-    - We decide how to recurse based on first two digits of the string.  
-    - If it is <= 26 and not in [10, 20], it has the number of possibilities of
-      substring s[1:] + s[2:]
-    - If it is 10 or 20 it has the same number of possibilities as the substring
-      starting at index 2 - s[2:]
-    - Otherwise it has the same number of possibilities as the substring
-      starting at index 1 - s[1:]
-
-    For all of these calls, we will have significant overlap in the substrings
-    we look at.  For that reason, we use a cache/dp to store the results for 
-    future lookup.  Before we do any recursive calls, we check the cache to see
-    if we can return a value immediately.
+    Use dynamic programming to store number of possibilities for strings ending
+    at index i.  Possibilities for ending at one index depends on the length 1
+    and 2 ints ending at that index, and possibilities for ending at previous
+    indices. 
     """
 
-    def helper(self, s):
-        global cache
-        """
-        :type s: str
-        :rtype: int
-        """
-        if s[0] == '0':
-            return 0
-        if len(s) == 1:
-            return 1
-        if len(s) == 2:
-            d = int(s)
-            return 2 if d <= 26 and s[1] != '0' else 1
-        if s in cache:
-            return cache[s]
-        else:
-            top = int(s[:2])
-            if top <= 26:
-                cache[s] = self.helper(s[1:]) + self.helper(s[2:])
-            else:
-                cache[s] = self.helper(s[1:])
-            return cache[s]
-
     def numDecodings(self, s):
-        global cache
-        for i in range(len(s) - 1):
-            if s[i] > '2' and s[i + 1] == '0':
-                return 0
-        ans = self.helper(s)
-        cache = {}
-        return ans
+        N = len(s)
+        pre_prev = 1
+        prev = 0 if s[0] == '0' else 1
+        for i in range(2, N+1):
+            newP = 0
+            first = int(s[i-1])
+            second = int(s[i-2:i])
+            if first > 0:
+                newP += prev
+            if second >= 10 and second <= 26:
+                newP += pre_prev
+            pre_prev, prev = prev, newP
+        return prev
